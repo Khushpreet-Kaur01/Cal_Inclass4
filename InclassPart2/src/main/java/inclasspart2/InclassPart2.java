818e9d92ecf9089f554eaf87b91dccfa268963b8
Khushpreet Kaur <kau15618@sheridancollege.ca>
2026-06-01 19:56:45 -0400

    Added addition and subtraction method

---
+++ InclassPart2/src/main/java/inclasspart2/InclassPart2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package inclasspart2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Khushpreet Kaur, 2026
+ */
+public class InclassPart2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+    }
+}
